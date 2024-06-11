75810cbd509820a63403e81872da61cb5e7a26fa
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-11 18:09:30 +0200

    feat: completed Milestone 3

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,10 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Lista Treni</h1>
-    <ul>
-        @foreach ($trains as $train)
-            <li>{{ $train->codice_treno}}</li>
-        @endforeach 
-    </ul>
+<div class="container mt-5">
+    <h1 class="mb-5">Lista Treni</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Azienda</th>
+                <th>Stazione di partenza</th>
+                <th>Stazione di arrivo</th>
+                <th>Orario di partenza</th>
+                <th>Orario di Arrivo</th>
+                <th>Codice treno</th>
+                <th>Numero di carrozze</th>
+                <th>In orario</th>
+                <th>Cancellato</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{ $train->id }}</td>
+                    <td>{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_di_partenza }}</td>
+                    <td>{{ $train->stazione_di_arrivo }}</td>
+                    <td>{{ $train->orario_di_partenza }}</td>
+                    <td>{{ $train->orario_di_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->numero_carrozze }}</td>
+                    <td>{{ $train->in_orario ? 'Si' : 'No' }}</td>
+                    <td>{{ $train->cancellato ? 'Si' : 'No' }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
