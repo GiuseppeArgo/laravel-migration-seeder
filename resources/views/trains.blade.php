a846c8bb6b1d0c0ac0cdef70fec25dfc1e6fa9cf
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-11 18:00:12 +0200

    feat: completed Milestone2

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
     <h1>Lista Treni</h1>
+    <ul>
+        @foreach ($trains as $train)
+            <li>{{ $train->codice_treno}}</li>
+        @endforeach 
+    </ul>
 @endsection
