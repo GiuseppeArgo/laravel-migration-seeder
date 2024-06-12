69bb47c86d259c962af76e5dd4eb1ffa3280fc86
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-12 16:13:53 +0200

    feat: updated project with faker filler

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker;
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,20 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+
+            $faker = Faker\Factory::create('it_IT');
+            $newTrain = new Train();
+            $newTrain->codice_treno = $faker->randomNumber(4, true);
+            $newTrain->azienda = $faker->word();
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->time();
+            $newTrain->orario_di_arrivo = $faker->time();
+            $newTrain->numero_carrozze = $faker->numberBetween(1, 10);
+            $newTrain->in_orario = ($faker->numberBetween(0, 1) === 1) ? true : false;
+            $newTrain->cancellato = ($faker->numberBetween(0, 1) === 1) ? true : false;
+            $newTrain->save();
+        }
     }
 }
